f58bdc588d512d19781a83ac99cd603b356af77d
0612me <0612me@users.noreply.github.com>
2018-01-10 21:36:12 +0800

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ $2 -eq 1 ]; then
-        rclone --config=/conf/rclone.conf copy "$3" gdrive:rclone
+        rclone --config=/cldata/rclone.conf copy "$3" gdrive:rclone
 	rm -f "$3"
 fi
